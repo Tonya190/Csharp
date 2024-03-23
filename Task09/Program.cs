1be0c879efe8645bfc3bb2860f9472d955b0f7eb
Tonya <mwmrrwm@gmail.com>
2024-03-23 14:57:20 +1000

    Seminar:TASK09

---
+++ Task09/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибоильшую цифру числа
+*/
+
+int number = new Random ().Next(10,100);
+System.Console.WriteLine(number);
+int first = number / 10;
+int second = number % 10;
+int max = first;
+
+if(second > max)
+{
+    max = second;
+}
+
+Console.WriteLine(max);
+
