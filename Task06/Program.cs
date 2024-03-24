55f0d7cd93f92396d2d67af26479d815cda046a4
Tonya <mwmrrwm@gmail.com>
2024-03-24 16:50:53 +1000

    Homework:Task3

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+*/
+Console.WriteLine("Введите число из отрезка [10,99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int first = number / 10;
+int two = number % 10;
+int max = first;
+if (two > max)
+{
+    max = two;
+}
+Console.WriteLine(max);
