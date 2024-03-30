8fca94e47260a81a6987b642db7d3ecef142a5d3
Tonya <mwmrrwm@gmail.com>
2024-03-30 12:37:16 +1000

    seminar3:task10

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задайте массив. Напишите программу которая определяет присутствует ли заданное число в массиве. Ответ да/нет
+*/
+int [] array = {1,2,3,4,5};
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int v = 0;
+foreach ( int i in array)
+{
+    if (i == number)
+    {
+        v=number;
+    }
+}
+if (v == number)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
