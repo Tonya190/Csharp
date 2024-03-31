a02483b6653a7ff5bb43591d1e8bf7afcad1b40b
Tonya <mwmrrwm@gmail.com>
2024-03-31 14:54:54 +1000

    seminar4:task1

---
+++ Task18/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными целыми числами. Определите количество четных чисел в этом массиве. 
+*/
+void FillArrray (int start, int end, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(start, end);
+    }
+}
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+}
+int CountEven (int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            result ++;
+        }
+    }
+    return result;
+}
+
+int [] user_arr = new int [10];
+FillArrray(1,100,user_arr);
+PrintArray(user_arr);
+System.Console.WriteLine();
+System.Console.WriteLine(CountEven(user_arr));
