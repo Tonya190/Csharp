26ae7b43c199e2950a2a28e0a28fc75bdcbd721d
Tonya <mwmrrwm@gmail.com>
2024-04-04 17:38:25 +1000

    task21

---
+++ Task21/Program.cs
@@ -0,0 +1,23 @@
+﻿void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100,1000);
+    }
+}
+
+ int EvenNumber(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            result ++;
+        }
+    }
+    return result;
+}
+int[] user_arr = new int [100,1000];
+System.Console.WriteLine(EvenNumber(user_arr));
+//Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
