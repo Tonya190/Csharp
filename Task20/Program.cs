23005f03897368701f0b093bdb9e0e60b0fb727d
Tonya <mwmrrwm@gmail.com>
2024-04-04 17:37:57 +1000

    task20

---
+++ Task20/Program.cs
@@ -0,0 +1,12 @@
+﻿static void Main(string[] args)
+{
+    int[] array = { 1, 3, 5, 6, 7, 8 };
+    foreach (int number in array)
+        {
+            Console.Write(number + ",");
+        }
+    System.Console.WriteLine();
+    Array.Reverse(array);
+    Console.WriteLine(String.Join(',', array));
+}
+//Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
