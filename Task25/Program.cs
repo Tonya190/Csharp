ed756bf433479433808f04a46ce3ed48583c9d73
Tonya <mwmrrwm@gmail.com>
2024-04-11 16:52:26 +1000

    homework6.3

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задайте произвольную строку. Выясните, является ли она палиндромом.
+*/
+string line = "love";
+bool result = IsPalindrom(line);
+System.Console.WriteLine(result ? "Да":"Нет");
+bool IsPalindrom(string line)
+{
+    string str = new string(line.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    return str.SequenceEqual(str.Reverse());
+}
