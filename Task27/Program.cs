c032f3762409c0f156a0291126539de4f23ad300
Tonya <mwmrrwm@gmail.com>
2024-04-11 16:29:53 +1000

    homework6.1

---
+++ Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+*/
+char [,] arr = new char [,] {{'l','o'},{'v','e'}};
+string result = CreateString(arr);
+Console.WriteLine(result);
+string CreateString(char[,] arr)
+{
+    string result = "";
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            result += arr[i, j];
+        }
+    }
+    return result;
+}
