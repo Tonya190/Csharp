1bdb3bfd2cb5ebdb7098da51e450350279e913aa
Tonya <mwmrrwm@gmail.com>
2024-04-15 15:30:46 +1000

    seminar7

---
+++ Task30/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+*/
+System.Console.WriteLine(PowUser(2,3));
+int PowUser(int a, int b)
+{
+    if (b == 1)
+    {
+        return a;
+    }
+    else
+    {
+        return a * PowUser(a, b-1);
+    }
+}
