9cf83cde0078a7c2456ecf4673a42f61146bf00d
Tonya <mwmrrwm@gmail.com>
2024-04-15 14:09:41 +1000

    seminar7

---
+++ Task28/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+*/
+int SumNumber(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return num % 10 + SumNumber(num/10);
+    }
+}
+System.Console.WriteLine(SumNumber(356));
