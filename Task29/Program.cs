aa101650dc2e9f173dcc2e9d71f3108ec544921f
Tonya <mwmrrwm@gmail.com>
2024-04-15 15:24:18 +1000

    seminar7

---
+++ Task29/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+Указание Использовать рекурсию. Не использовать цикл.
+*/
+System.Console.WriteLine("Введите число num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+IntegersNum(num);
+
+void IntegersNum(int num, int count = 1)
+{
+    if (num < count)
+    {
+        return;
+    }
+    else
+    {
+        System.Console.Write(count);
+        IntegersNum(num, count+1);
+    }
+}
