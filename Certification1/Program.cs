7b1d9697b2b70a5542d882e6d20efeb67784de15
Tonya <mwmrrwm@gmail.com>
2024-04-22 20:26:09 +1000

    certification1

---
+++ Certification1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы.
+*/
+int start = Convert.ToInt32(Console.ReadLine());
+int end = Convert.ToInt32(Console.ReadLine());
+IntegersNum(start, end);
+
+void IntegersNum(int start, int end)
+{
+    if (start > end)
+    {
+        return;
+    }
+    Console.Write(start);
+    IntegersNum(start + 1, end); 
+}
