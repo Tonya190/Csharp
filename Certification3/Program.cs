291de37ed46feb657fcb7f12d8715ae5e1441998
Tonya <mwmrrwm@gmail.com>
2024-04-22 20:27:18 +1000

    certification3

---
+++ Certification3/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+*/
+int[] arr = {1,2,3,4,5,6};
+ArrayReverse(arr,arr.Length-1);
+void ArrayReverse(int[] arr,int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+    System.Console.Write(arr[index]+ " ");
+    ArrayReverse(arr, index - 1);
+}
